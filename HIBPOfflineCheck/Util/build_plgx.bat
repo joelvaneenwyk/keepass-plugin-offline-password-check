29f6073e4fec60a3e245abc4e3c8cbf52fe10339
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2020-04-25 17:03:37 -0700

    Fixed plgx build script.

--- HIBPOfflineCheck/Util/build_plgx.bat
+++ HIBPOfflineCheck/Util/build_plgx.bat
@@ -1,30 +1,37 @@
-set PluginRoot=..
-set KeePassExe=%PluginRoot%\bin\Debug\KeePass.exe
-Set BuildPath=%~dp0
+@echo off
 
-xcopy /Y %PluginRoot%\Properties\AssemblyInfo.cs plgx\Properties\
-xcopy /Y %PluginRoot%\HIBPOfflineCheck.csproj plgx\
-xcopy /Y %PluginRoot%\HIBPOfflineCheck.sln plgx\
-xcopy /Y %PluginRoot%\HIBPOfflineCheckExt.cs plgx\
-xcopy /Y %PluginRoot%\HIBPOfflineCheckOptions.cs plgx\
-xcopy /Y %PluginRoot%\HIBPOfflineCheckOptions.Designer.cs plgx\
-xcopy /Y %PluginRoot%\HIBPOfflineCheckOptions.resx plgx\
-xcopy /Y %PluginRoot%\HIBPOfflineColumnProv.cs plgx\
-xcopy /Y %PluginRoot%\ProgressDisplay.cs plgx\
-xcopy /Y %PluginRoot%\ProgressDisplay.Designer.cs plgx\
-xcopy /Y %PluginRoot%\ProgressDisplay.resx plgx\
-xcopy /Y %PluginRoot%\Options.cs plgx\
-xcopy /Y %PluginRoot%\Resources\Nuvola\B48x48_KOrganizer.png plgx\Resources\Nuvola\
-xcopy /Y %PluginRoot%\Properties\Resources.Designer.cs plgx\Properties\
-xcopy /Y %PluginRoot%\Properties\Resources.resx plgx\Properties\
-xcopy /Y %PluginRoot%\BitStorage.cs plgx\
-xcopy /Y %PluginRoot%\BloomFilter.cs plgx\
-xcopy /Y %PluginRoot%\CreateBloomFilter.Designer.cs plgx\
-xcopy /Y %PluginRoot%\CreateBloomFilter.cs plgx\
-xcopy /Y %PluginRoot%\CreateBloomFilter.resx plgx\
+set PluginRoot=%~dp0..
+set KeePassExe=%PluginRoot%\Lib\KeePass.exe
+Set BuildPath=%PluginRoot%\Bin
+set OutputPath=%BuildPath%\plgx
 
-%KeePassExe% --plgx-create "%BuildPath%plgx"
+if not exist %OutputPath% mkdir %OutputPath%
 
-move /Y "%BuildPath%plgx.plgx" %PluginRoot%\HIBPOfflineCheck.plgx
+echo Copying source to output folder: %OutputPath%
+xcopy /Y %PluginRoot%\..\HIBPOfflineCheck.sln %OutputPath%\
+xcopy /Y %PluginRoot%\HIBPOfflineCheck.csproj %OutputPath%\
+xcopy /Y %PluginRoot%\HIBPOfflineCheckExt.cs %OutputPath%\
+xcopy /Y %PluginRoot%\HIBPOfflineCheckOptions.cs %OutputPath%\
+xcopy /Y %PluginRoot%\HIBPOfflineCheckOptions.Designer.cs %OutputPath%\
+xcopy /Y %PluginRoot%\HIBPOfflineCheckOptions.resx %OutputPath%\
+xcopy /Y %PluginRoot%\HIBPOfflineColumnProv.cs %OutputPath%\
+xcopy /Y %PluginRoot%\ProgressDisplay.cs %OutputPath%\
+xcopy /Y %PluginRoot%\ProgressDisplay.Designer.cs %OutputPath%\
+xcopy /Y %PluginRoot%\ProgressDisplay.resx %OutputPath%\
+xcopy /Y %PluginRoot%\Options.cs %OutputPath%\
+xcopy /Y %PluginRoot%\Properties\AssemblyInfo.cs %OutputPath%\Properties\
+xcopy /Y %PluginRoot%\Resources\Nuvola\B48x48_KOrganizer.png %OutputPath%\Resources\Nuvola\
+xcopy /Y %PluginRoot%\Properties\Resources.Designer.cs %OutputPath%\Properties\
+xcopy /Y %PluginRoot%\Properties\Resources.resx %OutputPath%\Properties\
+xcopy /Y %PluginRoot%\BitStorage.cs %OutputPath%\
+xcopy /Y %PluginRoot%\BloomFilter.cs %OutputPath%\
+xcopy /Y %PluginRoot%\CreateBloomFilter.Designer.cs %OutputPath%\
+xcopy /Y %PluginRoot%\CreateBloomFilter.cs %OutputPath%\
+xcopy /Y %PluginRoot%\CreateBloomFilter.resx %OutputPath%\
 
-rmdir /S /Q "%BuildPath%plgx
+echo Creating PLGX: '%KeePassExe% --plgx-create "%OutputPath%"'
+%KeePassExe% --plgx-create "%OutputPath%"
+
+move /Y "%BuildPath%\plgx.plgx" %BuildPath%\HIBPOfflineCheck.plgx
+
+rmdir /S /Q "%OutputPath%\"
